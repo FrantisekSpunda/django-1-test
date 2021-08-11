02a9d433864348d32d89e8725595e9d63db204f1
frantisek.spunda <spundafr@gmail.com>
2021-08-11 20:53:33 +0200

    section 6 (16): users profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def profiles(request):
-    context = {}
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills = profile.skill_set.exclude(description__exact="") # if skill don't have description don't add to variable
+    otherSkills = profile.skill_set.filter(description="")       # filter all skills that don't have description
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
+    return render(request, 'users/user_profile.html', context)
