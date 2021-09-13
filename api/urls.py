352208597f996626a32fd93777b9f6aa032e247e
frantisek.spunda <spundafr@gmail.com>
2021-09-13 22:18:39 +0200

    section 12: serializers

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('projects/', views.getProjects),
+    path('project/', views.getProjects),
     path('project/<str:pk>', views.getProject),
 ]
