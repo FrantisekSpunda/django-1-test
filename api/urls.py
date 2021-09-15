7304da472b11500b21160f831ea0b52129084604
frantisek.spunda <spundafr@gmail.com>
2021-09-15 17:54:39 +0200

    section 12: sending POST request

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    
+    # rest framework JSON authentication
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # api urls
     path('', views.getRoutes),
-    path('project/', views.getProjects),
-    path('project/<str:pk>', views.getProject),
+    path('projects/', views.getProjects),
+    path('projects/<str:pk>/', views.getProject),
+    path('projects/<str:pk>/vote/', views.projectVote),
 ]
