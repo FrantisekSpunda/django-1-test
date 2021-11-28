e74ac04b494f88936adbe40b8acd6940c7091fd2
frantisek.spunda <spundafr@gmail.com>
2021-11-28 12:16:45 +0100

    section 13(41): Tags managing

--- projects/forms.py
+++ projects/forms.py
@@ -5,10 +5,7 @@ from .models import Project, Review
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'featured_image', 'description', 'demo_link','source_link', 'tags']
-        widgets = {
-            'tags':forms.CheckboxSelectMultiple(),
-        }
+        fields = ['title', 'featured_image', 'description', 'demo_link','source_link']
 
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Project
+from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
 from .utils import searchProjects
 from devsearch.utils import paginateBlocks
@@ -49,6 +49,9 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
             messages.success(request, 'Project was added successfully!')
             return redirect('account')
     
@@ -63,9 +66,14 @@ def updateProject(request, pk):
     form = ProjectForm(instance=project)
 
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',',' ').split()
+
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
             messages.success(request, 'Project was updated successfully!')
             return redirect('account')
     
