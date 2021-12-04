f60324a1015cafc8cf7976b9f40bdfb81bd97858
frantisek.spunda <spundafr@gmail.com>
2021-12-04 13:05:18 +0100

    add to allowed_hosts

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e_!_h*lz1tj^r3%h6(wt_i=6&u&5w*wxy7!2czjtg$mt+l^o^5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','devsearch-by-spunda.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','devsearch-by-spunda.herokuapp.com', '.herokuapp.com']
 
 
 # Application definition
